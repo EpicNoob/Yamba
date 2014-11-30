ea5246eb3ddfad80dcf7eeba21502ae32908a91c
EpicNoob <Administrator@UMad>
2014-11-30 18:34:05 +0100

    TextCounter implemented

--- Yamba/src/com/example/yamba/StatusActivity.java
+++ Yamba/src/com/example/yamba/StatusActivity.java
@@ -1,14 +1,23 @@
 package com.example.yamba;
 
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 
@@ -18,6 +27,8 @@ public class StatusActivity extends Activity implements OnClickListener{
 	/** android XML Elements **/
 	private EditText editStatus;
 	private Button buttonTweet;
+	private TextView textCount;
+	private int defaultTextColor;
 	
 	/** Logfile parameters**/
 	private static final String TAG = "StatusAtivity";
@@ -28,11 +39,42 @@ public class StatusActivity extends Activity implements OnClickListener{
 		setContentView(R.layout.activity_status);
 		
 		/** link android xml elements with java elements here **/
-		editStatus = (EditText) findViewById(R.id.editStatus);
+		editStatus  = (EditText) findViewById(R.id.editStatus);
 		buttonTweet = (Button) findViewById(R.id.buttonTweet);
+		textCount   = (TextView) findViewById(R.id.textCount);
 		
 		/**At this view to the listener list of the tweetButton**/
 		buttonTweet.setOnClickListener(this);
+		
+		/** set the default color = green for the textCount element **/
+		defaultTextColor = textCount.getTextColors().getDefaultColor();
+		
+		/** implement the textChangeListener to editStatus **/
+		editStatus.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				int count = 140 - editStatus.length();
+				textCount.setText(Integer.toString(count));
+				textCount.setTextColor(Color.GREEN);
+				if(count < 10)
+					textCount.setTextColor(Color.RED);
+				else
+					textCount.setTextColor(defaultTextColor);
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				
+			}
+		});
 		
 	}
 
@@ -60,5 +102,34 @@ public class StatusActivity extends Activity implements OnClickListener{
 		/**what should happen when someone pushes the tweetButton is coded here**/
 		String status = editStatus.getText().toString();
 		Log.d(TAG, "onClicked with Status: " + status);
+		
+		/** push message to yamba **/
+		new PostTask().execute(status);
+	}
+	
+	/** implement threadlike class for yamba server communication features **/
+	private final class PostTask extends
+		AsyncTask<String, Void, String>{
+
+		@Override
+		protected String doInBackground(String... params) {
+			YambaClient yambaCloud = new YambaClient("student", "password");
+			try{
+				yambaCloud.postStatus(params[0]);
+				return "Successfully posted";
+			}catch (YambaClientException e){
+				Log.e(e.toString(), e.toString());
+				return "Failed to post to yamba servive";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+		
+		
+		
 	}
 }
